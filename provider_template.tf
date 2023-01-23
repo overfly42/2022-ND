19dc629d1910912560b35ac1852a8def1f1c1898
Christian Spreng <c.spreng@zoho.com>
2023-01-23 21:24:47 +0100

    Added template for provider

---
+++ provider_template.tf
@@ -0,0 +1,7 @@
+provider "azurerm" {
+    features {}
+    subscription_id = "<subscription_id>"
+    client_id       = "<client_id>"
+    client_secret   = "<client_secret>"
+    tenant_id       = "<tenant_id>"
+}
