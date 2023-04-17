f9fce46ae20fa0952a337593ecea2b77eb0784fb
Christian Spreng <c.spreng@zoho.com>
2023-04-17 22:11:50 +0200

    Added Locust tests

--- Project2/README.md
+++ Project2/README.md
@@ -6,3 +6,9 @@ URL: https://trello.com/b/R6aZ3kbq/udacity2022devopsprj2
 3. add config file ssh.config as config to ~/.ssh/
 4. Execute git clone
 
+# Load Test
+Result of localhost in json format: /locust.result
+How to execute for 100 users / for 10 seconds:
+locust --host http://localhost:5000 -f locustfile.py --users 100 --run-time 10 --headless --json
+
+
--- Project2/locustfile.py
+++ Project2/locustfile.py
@@ -1,1 +1,30 @@
 #Load test with locust
+from locust import HttpUser, task, between
+
+class TestUser(HttpUser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.data={
+            'CHAS': {
+                '0': 0,
+            },
+            'RM': {
+                '0': 6.575,
+            },
+            'TAX': {
+                '0': 296.0,
+            },
+            'PTRATIO': {
+                '0': 15.3,
+            },
+            'B': {
+                '0': 396.9,
+            },
+            'LSTAT': {
+                '0': 4.98,
+            }
+        }
+    @task
+    def get_prediction(self):
+        self.client.post('/predict',json=self.data)
+        
