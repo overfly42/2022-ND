8ee79b3b574aa732bbb7e94a8b376522a495e026
Christian Spreng <c.spreng@zoho.com>
2023-07-04 22:38:54 +0200

    Change vm resource group name

--- Project3/terraform/modules/vm/vm.tf
+++ Project3/terraform/modules/vm/vm.tf
@@ -14,7 +14,7 @@ resource "azurerm_network_interface" "test" {
 resource "azurerm_linux_virtual_machine" "myVM" {
   name                = "myVM"
   location            = "${var.location}"
-  resource_group_name = "${main.resource_group.resource_group_name}"
+  resource_group_name = "${var.resource_group}"
   size                = "Standard_DS2_v2"
   admin_username      = "adminuser"
   tags = {
