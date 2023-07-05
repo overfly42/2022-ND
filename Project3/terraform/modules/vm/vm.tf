123e996675f8950bcc244271fdd9b645a75a0065
Christian Spreng <c.spreng@zoho.com>
2023-07-05 20:54:05 +0200

    changed image to id

--- Project3/terraform/modules/vm/vm.tf
+++ Project3/terraform/modules/vm/vm.tf
@@ -25,16 +25,18 @@ resource "azurerm_linux_virtual_machine" "myVM" {
     username   = "adminuser"
     public_key = file("/home/christian/.ssh/id_rsa_udacity2022p3.pub")
   }
+  source_image_id = "/subscriptions/a7f4943c-0527-4918-98af-14af18d4cebf/resourceGroups/Udacity_cs/providers/Microsoft.Compute/galleries/vmgallery/images/test-vm"
   os_disk {
     caching           = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-  source_image_reference {
-    publisher = "cs"
+  
+ # source_image_reference {
+ #   publisher = "cs"
 #    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-focal"
+#    offer     = "0001-com-ubuntu-server-focal"
 #    sku       = "18.04-LTS"
-    sku = "20_04-lts-gen2"
-    version   = "1.0.0"
-  }
+#    sku = "20_04-lts-gen2"
+#    version   = "latest"
+#  }
 }
